19e8b663b1a85efb83fcc68c94be10fd7d3e6710
Shafaq-Juda <sjuda008@gmail.com>
2024-02-22 14:03:36 -0600

    overwrite routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
  
   # Routes for the Delivery resource:
+ root to: "deliveries#index"
 
   # CREATE
   post("/insert_delivery", { :controller => "deliveries", :action => "create" })
           
   # READ
-  get("/deliveries", { :controller => "deliveries", :action => "index" })
-  
+  #get("/deliveries", { :controller => "deliveries", :action => "index" })
+
+
   get("/deliveries/:path_id", { :controller => "deliveries", :action => "show" })
   
   # UPDATE
